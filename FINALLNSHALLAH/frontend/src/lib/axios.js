6ba12e7d4155ad9a72df76533380b93a0225bddc
Retaj12003 <161883290+Retaj12003@users.noreply.github.com>
2024-12-11 07:31:31 +0300

    Update axios.js

--- FINALLNSHALLAH/frontend/src/lib/axios.js
+++ FINALLNSHALLAH/frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "restaurant-ochre-alpha.vercel.app", 
+  baseURL: `${import.meta.env.VITE_API_URL}/api`, 
   withCredentials: true,
 });
 
