20cfb3bb2156990ab38a439c13d874f006494168
Retaj12003 <161883290+Retaj12003@users.noreply.github.com>
2024-12-11 07:08:47 +0300

    Update vite.config.js

--- FINALLNSHALLAH/frontend/vite.config.js
+++ FINALLNSHALLAH/frontend/vite.config.js
@@ -2,14 +2,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: `${import.meta.env.VITE_API_URL}`,  // Ensure VITE_API_URL is defined in .env
-        changeOrigin: true, // This will help with cross-origin requests
+        target: `${import.meta.env.VITE_API_URL}`, 
+        changeOrigin: true, 
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
