b6c5d69460608aea949b5a533bc935a7701c8f6e
Retaj12003 <161883290+Retaj12003@users.noreply.github.com>
2024-12-11 06:31:58 +0300

    Update vite.config.js

--- FINALLNSHALLAH/frontend/vite.config.js
+++ FINALLNSHALLAH/frontend/vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: `${import.meta.env.VITE_API_URL}/api`, 
-        changeOrigin: true, 
+        target: `${import.meta.env.VITE_API_URL}/api`,
+
         
       },
     },
